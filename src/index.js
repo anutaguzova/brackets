ae6f7b1c4788507112d0a5037fbab7378d025f73
anutaguzova <anuta-guzova@mail.ru>
2020-10-23 00:51:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = bracketsConfig.join().split(","),
+        stack = [],
+        open = [],
+        close = [];
+    for (let j = 0; j < arr.length; j++) {
+        if (j % 2 == 0) {
+            open.push(arr[j]);
+        } else {
+            close.push(arr[j]);
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+      if (open.indexOf(str[i]) !== -1) {
+        if (close.indexOf(str[i]) !== -1) {
+          if (stack[stack.length - 1] == str[i]) {
+            stack.pop();
+          } else {
+            stack.push(str[i]);
+          }
+        } else {
+          stack.push(str[i]);
+        }
+      } else {
+        if (stack[stack.length - 1] == open[close.indexOf(str[i])]) {
+                stack.pop();
+            } else {
+              return false
+            }
+        }
+    }
+    return stack.length == 0;
+};
